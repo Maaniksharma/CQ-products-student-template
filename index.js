41a8adbf84f48db651c3e56f04d30322d759748f
maanik sharma <manikss123456@gmail.com>
2024-03-06 12:08:42 +0530

    code simplification

--- index.js
+++ index.js
@@ -5,21 +5,18 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 const url = require('url');
-const config = require('./config.js');
 const handlers = require('./lib/handlers.js');
 const helpers = require('./lib/helpers.js');
 
 // the server should respond to all requests with a string
-//http server intatnination
+//http server creation
 let httpServer = http.createServer((req, res) => {
   Server(req, res);
 });
 
 //start the http server and have it listen on port 3000
-httpServer.listen(config.httpPort, () => {
-  console.log(
-    `http server is listening on port ${config.httpPort} in ${config.envName} mode`,
-  );
+httpServer.listen(3000, () => {
+  console.log(`http server is listening on port 3000`);
 });
 
 //server function
@@ -34,28 +31,26 @@ const Server = (req, res) => {
 
   let buffer = '';
   // storing body data in buffer
-  // req.on('data', (data) => {
-  //   buffer += data;
-  // });
+  req.on('data', (data) => {
+    buffer += data;
+  });
 
   req.on('end', () => {
     //static files handling with /public route and also handle the content type of headers to sent
     //choose the handler this should go.If one is not found then use the not found handler
     const chosenHandler = routing(trimmedPath);
     // make the data to pass to routes about quesry ,body, headers etc
-    // let data = {
-    //   trimmedPath: trimmedPath,
-    //   queryStringObject: QueryStringObject,
-    //   method: method,
-    //   headers: headers,
-    //   payload: helpers?.parseJsonToObject(buffer),
-    // };
+    let data = {
+      queryStringObject: QueryStringObject,
+      method: method,
+      payload: helpers.parseJsonToObject(buffer),
+    };
     //Route the request to the specific route according to the pathName
     chosenHandler(data, (statusCode, payload) => {
       //use the status code called back by the handler or default to 200 code
       //use the payload called back by the handler or default to an empty object
+      //sends the response and logs the request
     });
-    //sends the response and logs the request
   });
 };
 //routing todo:create routing for productdetails route
